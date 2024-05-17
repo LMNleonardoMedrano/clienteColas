afffb8e9ce6e814b79f22523917fdb656fb9c40c
PC-07 <PC-07@DESKTOP-UJU2H25>
2024-05-17 10:42:34 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clienteColas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue miFila = new Queue();
+            Console.Write("INGRESE EL NUMERO DE PERSONAS EN LA FILA DEL CAJERO: ");
+            int n = int.Parse(Console.ReadLine());
+            int tamañoFila = n;
+            int contador = 0;
+
+            while (contador < n)
+            {
+                Console.Write("NOMBRE DE LA PERSONA: ");
+                miFila.Enqueue(Console.ReadLine());
+                contador++;
+            }
+            Console.WriteLine("\nEL CAJERO ATENDIO A: ");
+
+            int numeroA = 1;
+
+            while (tamañoFila > 0)
+            {
+                Console.WriteLine(numeroA + ".-" + miFila.Dequeue());
+                tamañoFila--;
+                numeroA++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
